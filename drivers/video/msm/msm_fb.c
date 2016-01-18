8147b812d1cd5760173a26869916f0ac844b3181
remittor <remittor@gmail.com>
2016-01-18 14:06:11 +0300

    video: Fix backlight can't be lighten up issue in recovery mode
    from stock kernel

--- drivers/video/msm/msm_fb.c
+++ drivers/video/msm/msm_fb.c
@@ -1025,7 +1025,8 @@ void msm_fb_set_backlight(struct msm_fb_data_type *mfd, __u32 bkl_lvl)
 
 /* Remove qcom backlight mechanism,user our own */
 	if (!hack_lcd) {
-		if (!mfd->panel_power_on || !bl_updated) {
+		/*remove the bl_updated for the backlight in recovery can't light up*/
+		if (!mfd->panel_power_on) {
 			unset_bl_level = bkl_lvl;
 			return;
 		} else {
